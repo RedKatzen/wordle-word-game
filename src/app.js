bb031c7f242713c61190bb8cd769c41c756bac93
RedKatzen <gabrieldeavilaf@gmail.com.br>
2025-03-28 09:28:04 -0300

    fetching the api and treating the response (word)

--- src/app.js
+++ src/app.js
@@ -1,48 +1,58 @@
 const wordLength = 5;
 const rounds = 6;
-const letters = document.querySelectorAll('.scoreboard-letter');
-const loading = document.querySelector('.loading');
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loading = document.querySelector(".loading");
 
-// async function to initialize and call the API 
+// async function to initialize and call the API
 async function init() {
   // initial state of the game
   let currentRow = 0;
-  let currentGuess = '';
+  let currentGuess = "";
   let done = false;
   let isLoading = true;
   setLoading(isLoading);
 
+  const res = await fetch("https://words.dev-apis.com/word-of-the-day");
+  const { word: wordRes } = await res.json();
+  const word = wordRes.toUpperCase();
+  const wordParts = word.split("");
+
+  isLoading = false;
+  setLoading(isLoading);
+
   function commit() {
     currentRow++;
-    currentGuess = '';
+    currentGuess = "";
   }
 
   function addLetter(letter) {
-    if(currentGuess.length < wordLength) {
+    if (currentGuess.length < wordLength) {
       currentGuess += letter;
-      letters[currentRow * wordLength + currentGuess.length - 1].textContent = letter.toUpperCase();
+      letters[currentRow * wordLength + currentGuess.length - 1].textContent =
+        letter.toUpperCase();
     } else {
       currentGuess = currentGuess.substring(0, wordLength);
-      letters[currentRow * wordLength + currentGuess.length - 1].textContent = letter.toUpperCase();
+      letters[currentRow * wordLength + currentGuess.length - 1].textContent =
+        letter.toUpperCase();
     }
   }
-  
+
   // removing a letter by taking part of string (currentGuess) and attributing
   // by itself; assign the letter's position with blank string
   function removeLetter() {
-    if(currentGuess.length > 0) {
-      letters[currentRow * wordLength + currentGuess.length - 1].textContent = '';
+    if (currentGuess.length > 0) {
+      letters[currentRow * wordLength + currentGuess.length - 1].textContent =
+        "";
       currentGuess = currentGuess.substring(0, currentGuess.length - 1);
     }
   }
 
   document.addEventListener("keydown", ({ key }) => {
-    if(key === "Backspace") {
+    if (key === "Backspace") {
       removeLetter();
-    } else if(key === "Enter") {
-      if(currentGuess.length === 5)
-        commit();
-    } else if(isLetter(key)){
+    } else if (key === "Enter") {
+      if (currentGuess.length === 5) commit();
+    } else if (isLetter(key)) {
       addLetter(key);
     }
   });
@@ -56,7 +66,7 @@ function isLetter(letter) {
 
 // toggle the class 'show' to show/hide the loading txt
 function setLoading(isLoading) {
-  loading.classList.toggle("show", isLoading);
+  loading.classList.toggle("hidden", !isLoading);
 }
 
 // function to count the number of times a letter appears in an array
